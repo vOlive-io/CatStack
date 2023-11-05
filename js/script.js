a482b80de3d4a944e03ef1dc6880300f98c44762
Hanna <83796834+PinkQween@users.noreply.github.com>
2023-11-05 15:30:34 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2137,7 +2137,7 @@ document.addEventListener("DOMContentLoaded", function () {
       // Send the token to your server for processing
       const tokenData = { token: token.id };
       // Replace with your server endpoint for payment processing
-      fetch('https://cats-buy.cendrive.com/create-payment-intent-500000-kit-coins', {
+      fetch('https://cats.cendrive.com/create-payment-intent-500000-kit-coins', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
